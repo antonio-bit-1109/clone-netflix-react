8d68d09655a73d487f511d0bc042f81bab71995a
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-22 20:19:27 +0100

    animation

--- src/components/SingleFilm.jsx
+++ src/components/SingleFilm.jsx
@@ -2,9 +2,12 @@ import { Component } from "react";
 import Col from "react-bootstrap/esm/Col";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
+import { Button } from "react-bootstrap";
 
 class SingleFilm extends Component {
-    state = {};
+    state = {
+        seeComments: false,
+    };
 
     render() {
         const { film } = this.props;
@@ -19,15 +22,32 @@ class SingleFilm extends Component {
             <Col className="h-50" sm={12} md={6} lg={4} xl={2}>
                 <Card
                     className="single-dimensions my-3"
-                    style={{ maxHeight: "100%", minWidth: "200px", objectFit: "cover" }}
+                    style={{
+                        maxHeight: "100%",
+                        minWidth: "200px",
+                        objectFit: "cover",
+                        /*  display: this.state.seeComments ? "block" : "none", */
+                    }}
+                    onClick={() => this.setState({ seeComments: !this.state.seeComments })}
                 >
                     <Card.Img variant="top" src={film.Poster} className="single-dimensions h-100" />
                     <div>
-                        <ListGroup className="text-center">
-                            <ListGroup.Item>{film.Title}</ListGroup.Item>
+                        <ListGroup
+                            className="text-center"
+                            style={{
+                                display: this.state.seeComments ? "block" : "none",
+                                animation: this.state.seeComments
+                                    ? "scale-up-top .4s cubic-bezier(.39,.575,.565,1.000) both"
+                                    : "scale(0)",
+                            }}
+                        >
+                            <ListGroup.Item className="fw-bold">{film.Title}</ListGroup.Item>
                             <ListGroup.Item>{film.Type}</ListGroup.Item>
                             <ListGroup.Item>{film.Year}</ListGroup.Item>
                             <ListGroup.Item>{film.imdbID}</ListGroup.Item>
+                            <Button variant="info" className="text-light m-2">
+                                Lascia un commento
+                            </Button>
                         </ListGroup>
                     </div>
                 </Card>
