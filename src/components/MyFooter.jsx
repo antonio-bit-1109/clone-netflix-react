bd71fc2c46735cd2761145fa9d0a7d9d66b8e226
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-22 16:40:01 +0100

    minimamente decente

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -8,66 +8,68 @@ const MyFooter = () => {
         <Container fluid className="text-secondary">
             <footer className="mt-5">
                 <Row>
-                    <div
-                        id="this"
-                        className="d-flex justify-content-center text-secondary justify-content-start m-auto gap-4 mb-2 mt-5 fs-3"
-                    >
-                        <div>
-                            <Meta />
+                    <Col>
+                        <div
+                            id="this"
+                            className="d-flex justify-content-center text-secondary justify-content-start m-auto gap-4 mb-3 mt-5 fs-3"
+                        >
+                            <div>
+                                <Meta />
+                            </div>
+                            <div>
+                                <Instagram />
+                            </div>
+                            <div>
+                                <Twitter />
+                            </div>
+                            <div>
+                                <Youtube />
+                            </div>
                         </div>
-                        <div>
-                            <Instagram />
-                        </div>
-                        <div>
-                            <Twitter />
-                        </div>
-                        <div>
-                            <Youtube />
-                        </div>
-                    </div>
+                    </Col>
                 </Row>
-                <Row>
+                <Row sm={1} md={2}>
                     <div
                         id="that"
                         className="d-flex text-secondary justify-content-start m-auto gap-4 flex-wrap flex-md-nowrap"
                     >
                         <Col>
                             <div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
+                                <div className="mb-1">Audio and subtitles</div>
+                                <div className="mb-1">Media Center</div>
+                                <div className="mb-1">Privacy</div>
+                                <div className="mb-1">Contact Us</div>
                             </div>
                         </Col>
                         <Col>
                             <div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
+                                <div className="mb-1">Audio and subtitles</div>
+                                <div className="mb-1">Media Center</div>
+                                <div className="mb-1">Privacy</div>
+                                <div className="mb-1">Contact Us</div>
                             </div>
                         </Col>
                         <Col>
                             <div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
+                                <div className="mb-1">Audio and subtitles</div>
+                                <div className="mb-1">Media Center</div>
+                                <div className="mb-1">Privacy</div>
+                                <div className="mb-1">Contact Us</div>
                             </div>
                         </Col>
                         <Col>
                             <div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
-                                <div className="mb-1">Lorem, ipsum dolor.</div>
+                                <div className="mb-1">Audio and subtitles</div>
+                                <div className="mb-1">Media Center</div>
+                                <div className="mb-1">Privacy</div>
+                                <div className="mb-1">Contact Us</div>
                             </div>
                         </Col>
                     </div>
                 </Row>
                 <Row>
                     <Col xs={12}>
-                        <div id="those" className="d-flex text-secondary justify-content-center m-auto gap-4 mb-5">
+                        <div id="those" className="d-flex text-secondary justify-content-center m-auto gap-4">
                             {" "}
                             <button
                                 type="button"
@@ -76,10 +78,9 @@ const MyFooter = () => {
                                 Service Code
                             </button>
                         </div>
-                    </Col>
-                    <Col xs={12}>
-                        {" "}
-                        <div>©1999-2009 Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, vel?</div>{" "}
+                        <div className="d-flex justify-content-center mb-5">
+                            <div>©1999-2009 Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, vel?</div>{" "}
+                        </div>
                     </Col>
                 </Row>
             </footer>
