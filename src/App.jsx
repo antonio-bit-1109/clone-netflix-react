426f62efdf8f993c4662608fa28320805ecf3fb7
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-22 16:31:03 +0100

    async funtion nella chiamata fetch

--- src/App.jsx
+++ src/App.jsx
@@ -10,52 +10,51 @@ class App extends Component {
     state = {
         filmsBySaga: {
             batman: [],
-            superman: [],
+            hulk: [],
             matrix: [],
         },
     };
 
-    fetchFilms = (NomeSaga) => {
+    fetchFilms = async (NomeSaga) => {
         const options = {
             method: "GET",
             headers: {},
         };
 
-        fetch(`http://www.omdbapi.com/?s=${NomeSaga}&apikey=195f13a4`, options)
-            .then((response) => {
-                if (!response.ok) {
-                    if (response.status > 400 && response.status < 500) {
-                        if (response.status === 429) {
-                            throw new Error("429 INFAME PER TE TANTE COSE BRUTTE");
-                        } else {
-                            throw new Error("STAI CAPPELLANDO , RIGUARDA QUELLO CHE HAI SCRITTO");
-                        }
-                    }
-                    if (response.status > 500 && response.status < 600) {
-                        throw new Error("SERVER SPOMPATO, NON FUNZIA??");
-                    }
-                } else {
-                    return response.json();
-                }
-            })
-            .then((data) => {
-                console.log(data);
-                this.setState(() => ({
-                    filmsBySaga: {
-                        ...this.state.filmsBySaga,
-                        [NomeSaga]: data.Search || [],
-                    },
-                }));
-            })
+        try {
+            const response = await fetch(`http://www.omdbapi.com/?s=${NomeSaga}&apikey=195f13a4`, options);
 
-            .catch((err) => {
-                console.log(err);
-            });
+            if (!response.ok) {
+                if (response.status > 400 && response.status < 500) {
+                    if (response.status === 429) {
+                        throw new Error("429 INFAME PER TE TANTE COSE BRUTTE");
+                    } else {
+                        throw new Error("STAI CAPPELLANDO, RIGUARDA QUELLO CHE HAI SCRITTO");
+                    }
+                }
+                if (response.status > 500 && response.status < 600) {
+                    throw new Error("SERVER SPOMPATO, NON FUNZIA??");
+                }
+            }
+
+            const data = await response.json();
+
+            console.log(data);
+
+            this.setState((prevState) => ({
+                filmsBySaga: {
+                    ...prevState.filmsBySaga,
+                    [NomeSaga]: data.Search,
+                },
+            }));
+        } catch (error) {
+            console.log(error);
+        }
     };
 
-    componentDidMount() {
+    async componentDidMount() {
         this.fetchFilms("batman");
-        this.fetchFilms("superman");
+        this.fetchFilms("hulk");
         this.fetchFilms("matrix");
     }
 
@@ -65,7 +64,7 @@ class App extends Component {
                 <NavBar />
                 <SecondSection />
                 <FilmSection filmSection={this.state.filmsBySaga.batman} />
-                <FilmSection filmSection={this.state.filmsBySaga.superman} />
+                <FilmSection filmSection={this.state.filmsBySaga.hulk} />
                 <FilmSection filmSection={this.state.filmsBySaga.matrix} />
                 <MyFooter />
             </div>
--- src/components/FilmSection.jsx
+++ src/components/FilmSection.jsx
@@ -4,12 +4,54 @@ import Row from "react-bootstrap/esm/Row";
 import SingleFilm from "./SingleFilm";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import { Button } from "react-bootstrap";
 
 class FilmSection extends Component {
-    state = {};
+    state = {
+        inputValue: "",
+        arrayOfFilms: null,
+    };
+
+    handleChange(event) {
+        this.setState({ ...this.state, inputValue: event.target.value });
+    }
+
+    handleClick = () => {
+        this.FetchAGet(this.state.inputValue);
+    };
+
+    FetchAGet(value) {
+        const options = {
+            method: "GET",
+            headers: {},
+        };
+
+        fetch(`http://www.omdbapi.com/?s=${value}&apikey=195f13a4`, options)
+            .then((response) => {
+                if (!response.ok) {
+                    if (response.status > 400 && response.status < 500) {
+                        if (response.status === 429) {
+                            throw new Error("429 INFAME PER TE TANTE COSE BRUTTE");
+                        } else {
+                            throw new Error("STAI CAPPELLANDO , RIGUARDA QUELLO CHE HAI SCRITTO");
+                        }
+                    }
+                    if (response.status > 500 && response.status < 600) {
+                        throw new Error("SERVER SPOMPATO, NON FUNZIA??");
+                    }
+                } else {
+                    console.log(response);
+                    return response.json();
+                }
+            })
+            .then((data) => {
+                console.log(data);
+                this.setState({ arrayOfFilms: data.Search });
+            });
+    }
 
     render() {
-        const { filmSection } = this.props;
+        const { filmSection } = this.props; /* array di films proveniente da app */
         console.log(filmSection);
 
         return (
@@ -20,12 +62,24 @@ class FilmSection extends Component {
                             placeholder="inserisci il titolo di un film..."
                             aria-label="Username"
                             aria-describedby="basic-addon1"
+                            onChange={(event) => this.handleChange(event)}
                         />
+                        <Button type="button" variant="warning" onClick={this.handleClick}>
+                            Press Here
+                        </Button>
                     </InputGroup>
                     <Row>
-                        {filmSection.slice(0, 6).map((film, index) => (
-                            <SingleFilm key={`serie-di-film${index}`} film={film} index={index} />
-                        ))}
+                        {!this.state.arrayOfFilms
+                            ? filmSection
+                                  .slice(0, 6)
+                                  .map((film, index) => (
+                                      <SingleFilm key={`serie-di-film${index}`} film={film} index={index} />
+                                  ))
+                            : this.state.arrayOfFilms
+                                  .slice(0, 6)
+                                  .map((film, index) => (
+                                      <SingleFilm key={`serie-di-film${index}`} film={film} index={index} />
+                                  ))}
                     </Row>
                 </div>
             </Container>
