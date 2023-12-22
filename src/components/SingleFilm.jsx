cde336f27ba8d9993f05a0a0e6f39a88d93f984d
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2023-12-22 19:24:09 +0100

    gg

--- src/components/SingleFilm.jsx
+++ src/components/SingleFilm.jsx
@@ -1,12 +1,15 @@
 import { Component } from "react";
 import Col from "react-bootstrap/esm/Col";
 import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 class SingleFilm extends Component {
     state = {};
 
     render() {
         const { film } = this.props;
+        console.log(film);
+
         const { index } = this.props;
 
         console.log(film);
@@ -19,6 +22,14 @@ class SingleFilm extends Component {
                     style={{ maxHeight: "100%", minWidth: "200px", objectFit: "cover" }}
                 >
                     <Card.Img variant="top" src={film.Poster} className="single-dimensions h-100" />
+                    <div>
+                        <ListGroup className="text-center">
+                            <ListGroup.Item>{film.Title}</ListGroup.Item>
+                            <ListGroup.Item>{film.Type}</ListGroup.Item>
+                            <ListGroup.Item>{film.Year}</ListGroup.Item>
+                            <ListGroup.Item>{film.imdbID}</ListGroup.Item>
+                        </ListGroup>
+                    </div>
                 </Card>
             </Col>
         );
