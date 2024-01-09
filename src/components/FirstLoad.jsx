195d3b4157fcac8aa386a64c43fd634151bca820
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-09 13:22:08 +0100

    gg

--- src/components/FirstLoad.jsx
+++ src/components/FirstLoad.jsx
@@ -1,21 +1,18 @@
-import { Component } from "react";
 import { Container } from "react-bootstrap";
 
-class FirstLoad extends Component {
-    render() {
-        return (
-            <Container>
-                <div
-                    id="alert-iniziale"
-                    className="text-light d-flex align-items-center justify-content-center fs-3"
-                    style={{ minHeight: "220px" }}
-                >
-                    {" "}
-                    ☝ Hey! Seleziona uno dei link rossi in alto per iniziare la navigazione! ☝{" "}
-                </div>
-            </Container>
-        );
-    }
-}
+const FirstLoad = () => {
+    return (
+        <Container>
+            <div
+                id="alert-iniziale"
+                className="text-light d-flex align-items-center justify-content-center fs-3"
+                style={{ minHeight: "220px" }}
+            >
+                {" "}
+                ☝ Hey! Seleziona uno dei link rossi in alto per iniziare la navigazione! ☝{" "}
+            </div>
+        </Container>
+    );
+};
 
 export default FirstLoad;
