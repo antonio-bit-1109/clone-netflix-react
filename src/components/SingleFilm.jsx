8fa487d8d3a4868cad10d52c32030a14bf8a5a4a
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-09 13:14:34 +0100

    refactor singlefilm

--- src/components/SingleFilm.jsx
+++ src/components/SingleFilm.jsx
@@ -1,35 +1,53 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import Col from "react-bootstrap/esm/Col";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import FormComponent from "./FormComponent";
 
-class SingleFilm extends Component {
-    state = {
+const SingleFilm = (props) => {
+    /* state = {
         seeComments: false,
         fullComment: { email: "", name: "", surname: "", adult: false, comment: "", dateTime: "", phone: "" },
         submitted: null,
-    };
+    }; */
 
-    handleSubmit = (event) => {
+    const { film } = props;
+    console.log(film);
+
+    const { index } = props;
+
+    console.log(film);
+    console.log(index);
+
+    const [seeComments, setSeeComments] = useState(false);
+    const [fullComment, setFullComment] = useState({
+        email: "",
+        name: "",
+        surname: "",
+        adult: false,
+        comment: "",
+        dateTime: "",
+        phone: "",
+    });
+    const [submitted, setSubmitted] = useState(null);
+
+    const handleSubmit = (event) => {
         event.preventDefault();
 
-        this.postAFetch();
+        postAFetch();
     };
 
-    handleFullComment = (propertyName, propertyValue) => {
-        this.setState({
-            fullComment: { ...this.state.fullComment, [propertyName]: propertyValue /* event.target.checked */ },
-        });
+    const handleFullComment = (propertyName, propertyValue) => {
+        setFullComment({ ...fullComment, [propertyName]: propertyValue /* event.target.checked */ });
     };
 
-    postAFetch() {
+    const postAFetch = () => {
         const options = {
             method: "POST",
             headers: {
                 "content-type": "application/json",
             },
-            body: JSON.stringify(this.state.fullComment),
+            body: JSON.stringify(fullComment),
         };
 
         fetch("https://striveschool-api.herokuapp.com/api/reservation", options)
@@ -37,7 +55,7 @@ class SingleFilm extends Component {
                 console.log(response);
                 if (!response.ok) {
                     /*  */
-                    this.setState({ submitted: false });
+                    setSubmitted(false);
 
                     if (response.status > 400 && response.status < 500) {
                         if (response.status === 429) {
@@ -51,68 +69,56 @@ class SingleFilm extends Component {
                         console.log(response.status);
                         console.log("SPEDITO, DAIE!");
                     }
-                    this.setState({ submitted: true });
+                    setSubmitted(true);
                 }
             })
             .catch((err) => console.error(err));
-    }
-
-    handleinputValue = (event) => {
-        this.setState({ fullComment: { ...this.state.fullComment, comment: event.target.value } });
     };
 
-    render() {
-        const { film } = this.props;
-        console.log(film);
+    const handleinputValue = (event) => {
+        setFullComment({ ...fullComment, comment: event.target.value });
+    };
 
-        const { index } = this.props;
+    return (
+        <Col className="h-50" sm={12} md={6} lg={4} xl={2}>
+            {" "}
+            <Card className="my-3 h-100">
+                <Card.Img
+                    variant="top"
+                    src={film.Poster}
+                    className="gallery-img"
+                    onClick={() => setSeeComments(!seeComments)}
+                />
 
-        console.log(film);
-        console.log(index);
+                <div>
+                    <ListGroup
+                        className="text-center p-3"
+                        style={{
+                            display: seeComments ? "block" : "none",
+                            animation: seeComments ? "scale-up-top .4s cubic-bezier(.39,.575,.565,1.000) both" : "none",
+                        }}
+                    >
+                        <ListGroup.Item className="fw-bold">{film.Title}</ListGroup.Item>
+                        <ListGroup.Item>{film.Type}</ListGroup.Item>
+                        <ListGroup.Item>{film.Year}</ListGroup.Item>
+                        <ListGroup.Item>{film.imdbID}</ListGroup.Item>
 
-        return (
-            <Col className="h-50" sm={12} md={6} lg={4} xl={2}>
-                {" "}
-                <Card className="my-3 h-100">
-                    <Card.Img
-                        variant="top"
-                        src={film.Poster}
-                        className="gallery-img"
-                        onClick={() => this.setState({ seeComments: !this.state.seeComments })}
-                    />
-
-                    <div>
-                        <ListGroup
-                            className="text-center p-3"
-                            style={{
-                                display: this.state.seeComments ? "block" : "none",
-                                animation: this.state.seeComments
-                                    ? "scale-up-top .4s cubic-bezier(.39,.575,.565,1.000) both"
-                                    : "none",
-                            }}
-                        >
-                            <ListGroup.Item className="fw-bold">{film.Title}</ListGroup.Item>
-                            <ListGroup.Item>{film.Type}</ListGroup.Item>
-                            <ListGroup.Item>{film.Year}</ListGroup.Item>
-                            <ListGroup.Item>{film.imdbID}</ListGroup.Item>
-
-                            <div>
-                                {/* Form */}
-                                <FormComponent
-                                    film={film}
-                                    handleinputValue={this.handleinputValue}
-                                    handleSubmit={this.handleSubmit}
-                                    handleFullComment={this.handleFullComment}
-                                    adult={this.state.fullComment.adult}
-                                    submitted={this.state.submitted}
-                                />
-                            </div>
-                        </ListGroup>
-                    </div>
-                </Card>
-            </Col>
-        );
-    }
-}
+                        <div>
+                            {/* Form */}
+                            <FormComponent
+                                film={film}
+                                handleinputValue={handleinputValue}
+                                handleSubmit={handleSubmit}
+                                handleFullComment={handleFullComment}
+                                adult={fullComment.adult}
+                                submitted={submitted}
+                            />
+                        </div>
+                    </ListGroup>
+                </div>
+            </Card>
+        </Col>
+    );
+};
 
 export default SingleFilm;
